7f312175061ffcad227576e17b1176285fcf0ef7
IPerhapsCode <paulepaquin2002@gmail.com>
2024-03-11 12:17:44 -0400

    cours de prog objet annexe 6

--- Projets/Annexe6/app/src/main/java/com/example/annexe6/MainActivity.java
+++ Projets/Annexe6/app/src/main/java/com/example/annexe6/MainActivity.java
@@ -12,49 +12,80 @@ import android.graphics.RectF;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     ConstraintLayout parent;
-    Paint paint;
-    Canvas canvas;
     SurfaceDessin surfaceDessin;
-    RectF oval;
+    Button button;
+    Ecouteur ec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ec = new Ecouteur();
+
         parent = findViewById(R.id.Parent);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(ec);
 
         DisplayMetrics test =
         getResources().getDisplayMetrics();
 
-        paint = new Paint();
-        paint.setAntiAlias(true);
-        canvas = new Canvas();
-        oval = new RectF(360.f, 25.f, 520.f, 185.f);
-
         surfaceDessin = new SurfaceDessin(this);
-        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(test.widthPixels, test.heightPixels);
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams((int)(test.widthPixels * 0.5),(int)(test.heightPixels * 0.1));
         surfaceDessin.setLayoutParams(layoutParams);
         surfaceDessin.setBackgroundColor(Color.BLACK);
 
         parent.addView(surfaceDessin);
     }
 
+    private class Ecouteur implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v) {
+            surfaceDessin.setPrint();
+            surfaceDessin.invalidate();
+            switch((int)Math.round(Math.random() * 2))
+            {
+                case 1 : button.setBackgroundColor(getResources().getColor(R.color.blue)); break;
+                case 2 : button.setBackgroundColor(getResources().getColor(R.color.red)); break;
+                case 0 : button.setBackgroundColor(getResources().getColor(R.color.yellow)); break;
+            }
+
+        }
+    }
+
     private class SurfaceDessin extends View
     {
+        private Paint paint;
+        private boolean print;
+        private RectF oval;
+
         public SurfaceDessin(Context context)
         {
             super(context);
+
+            print = false;
+            paint = new Paint();
+            paint.setAntiAlias(true);
+            oval = new RectF(360.f, 25.f, 520.f, 185.f);
+        }
+
+        public void setPrint()
+        {
+            print = !print;
         }
 
         @SuppressLint("ResourceAsColor")
         @Override
         protected void onDraw(Canvas canvas)
         {
+            super.onDraw(canvas);
+
             paint.setColor(Color.GREEN);
             paint.setStyle(Paint.Style.FILL);
             canvas.drawCircle(80, 100, 80, paint);
@@ -64,13 +95,16 @@ public class MainActivity extends AppCompatActivity {
             paint.setStrokeWidth(10);
             canvas.drawCircle(250, 100, 80, paint);
 
-            paint.setStyle(Paint.Style.FILL);
-            paint.setColor(getResources().getColor(R.color.yellow));
-            canvas.drawArc(oval, 0.f, 120.f, true, paint);
-            paint.setColor(getResources().getColor(R.color.blue));
-            canvas.drawArc(oval, 120.f, 120.f, true, paint);
-            paint.setColor(getResources().getColor(R.color.red));
-            canvas.drawArc(oval, 240.f, 120.f, true, paint);
+            if(this.print)
+            {
+                paint.setStyle(Paint.Style.FILL);
+                paint.setColor(getResources().getColor(R.color.yellow));
+                canvas.drawArc(oval, 0.f, 120.f, true, paint);
+                paint.setColor(getResources().getColor(R.color.blue));
+                canvas.drawArc(oval, 120.f, 120.f, true, paint);
+                paint.setColor(getResources().getColor(R.color.red));
+                canvas.drawArc(oval, 240.f, 120.f, true, paint);
+            }
         }
     }
-}
+}//déplacer centre, autre méthode couleur, invalidate and redraw
