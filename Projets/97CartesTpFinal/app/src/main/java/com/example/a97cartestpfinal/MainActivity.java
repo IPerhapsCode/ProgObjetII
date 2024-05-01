23c28d2b917b0b2c0f2a2a46b6088b2184e13d1c
IPerhapsCode <paulepaquin2002@gmail.com>
2024-04-30 22:45:34 -0400

    layout complété pour play activity tp final

--- Projets/97CartesTpFinal/app/src/main/java/com/example/a97cartestpfinal/MainActivity.java
+++ Projets/97CartesTpFinal/app/src/main/java/com/example/a97cartestpfinal/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.a97cartestpfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Appliquer un style à une nouvelle vu:
+//        ContextThemeWrapper themedContext = new ContextThemeWrapper(this, R.style.MyTextViewStyle);
+//        TextView textView = new TextView(themedContext);
+
+        //Changer la couleur du background et l'opacité de la vu
+//        TextView testing = findViewById(R.id.textView8);
+//        GradientDrawable bruh = (GradientDrawable) testing.getBackground();
+//        bruh.setColor(Color.rgb(255, 0,0));
+//        testing.setAlpha(0);
     }
 }
